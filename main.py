db4d0b2f5b6033fed8e104d098859d8a5a02601b
Jay <59456814+Plazor26@users.noreply.github.com>
2021-02-08 18:34:28 +0530

    Updated main and camera files for new features

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, Response
 from camera import VideoCamera
+from camera import PhotoCamera
+appVideo = Flask(__name__)
 
-
-app = Flask(__name__)
-
-@app.route('/')
+@appVideo.route('/')
 def index():
     return render_template('index.html')
 
@@ -14,10 +13,33 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-@app.route('/video_feed')
+@appVideo.route('/video_feed')
 def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+
+appImg = Flask(__name__)
+
+@appImg.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@appImg.route('/video_feed')
+def video_feed():
+    return Response(gen(PhotoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+print("Enter your choice:")
+choice=int(input())
+if(choice==1):
+    if __name__ == '__main__':
+        appVideo.run(host='0.0.0.0', debug=True)
+else:
+    if __name__ == '__main__':
+        appImg.run(host='0.0.0.0', debug=True)
